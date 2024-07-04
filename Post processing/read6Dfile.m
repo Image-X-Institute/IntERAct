bf6afa4bd57c63a4788064d1253b5971b90c05b9
ChrisKuban <166461724+ChrisKuban@users.noreply.github.com>
2024-07-04 15:13:16 +1000

    Update read6Dfile.m

--- Post processing/read6Dfile.m
+++ Post processing/read6Dfile.m
@@ -1,13 +1,10 @@
 function [traces6DTime, tracesAP] = read6Dfile(filename)
-    % Ouvrir le fichier
     fileID = fopen(filename, 'r');
     
-    % Lire les données du fichier avec 'textscan'
     traces6D = textscan(fileID, '%f %f %f %f %f %f %f');
-    % Fermer le fichier
+
     fclose(fileID);
     
-    % Extraire les colonnes nécessaires
-    traces6DTime = traces6D{1}; % Première colonne
-    tracesAP = traces6D{4};     % Quatrième colonne
+    traces6DTime = traces6D{1}; 
+    tracesAP = traces6D{4};    
 end
